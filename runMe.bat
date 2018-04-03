848ecc3bdba584ee4942c0526c4c31d0134a3045
Md Shoaib Alam -X (mdsalam - ACCENTURE LLP at Cisco) <mdsalam@partnet.cisco.com>
2018-04-03 17:43:28 +0530

    add bat file

---
+++ runMe.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Welcome !
+pause
+cd target
+java -jar com.todos.api-0.0.1-SNAPSHOT.jar
--- target/runMe.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Welcome !
-pause
-
-java -jar com.todos.api-0.0.1-SNAPSHOT.jar
